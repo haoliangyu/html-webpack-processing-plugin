3c5b0e5c4b4f90a2f1a1008737bbc4d416e54871
haoliangyu <njkon@foxmail.com>
2016-11-02 10:10:14 -0400

    change to es6

--- src/index.js
+++ src/index.js
@@ -1,49 +1,52 @@
 const assert = require('assert');
 
-function HtmlWebpackProcessingPlugin (options) {
-  assert.equal(options, undefined, 'The HtmlWebpackProcessingPlugin does not accept any options');
+class HtmlWebpackProcessingPlugin {
+
+  constructor(options) {
+    assert.equal(options, undefined, 'The HtmlWebpackProcessingPlugin does not accept any options');
+  }
+
+  apply(compiler) {
+    let self = this;
+
+    compiler.plugin('compilation', compilation => {
+      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
+        self.preProcessing(htmlPluginData, callback);
+      });
+    });
+
+    compiler.plugin('compilation', compilation => {
+      compilation.plugin('html-webpack-plugin-after-html-processing', (htmlPluginData, callback) => {
+        self.postProcessing(htmlPluginData, callback);
+      });
+    });
+  }
+
+  preProcessing(htmlPluginData, callback) {
+    if (typeof htmlPluginData.plugin.options.preProcessing === 'function') {
+      try {
+        htmlPluginData.html = htmlPluginData.plugin.options.preProcessing(htmlPluginData.html);
+        callback(null, htmlPluginData);
+      } catch(err) {
+        callback(err);
+      }
+    } else {
+      callback(null, htmlPluginData);
+    }
+  }
+
+  postProcessing(htmlPluginData, callback) {
+    if (typeof htmlPluginData.plugin.options.postProcessing === 'function') {
+      try {
+        htmlPluginData.html = htmlPluginData.plugin.options.postProcessing(htmlPluginData.html);
+        callback(null, htmlPluginData);
+      } catch(err) {
+        callback(err);
+      }
+    } else {
+      callback(null, htmlPluginData);
+    }
+  }
 }
-
-HtmlWebpackProcessingPlugin.prototype.apply = function (compiler) {
-  let self = this;
-
-  compiler.plugin('compilation', (compilation) => {
-    compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
-      self.preProcessing(htmlPluginData, callback);
-    });
-  });
-
-  compiler.plugin('compilation', (compilation) => {
-    compilation.plugin('html-webpack-plugin-after-html-processing', (htmlPluginData, callback) => {
-      self.postProcessing(htmlPluginData, callback);
-    });
-  });
-};
-
-HtmlWebpackProcessingPlugin.prototype.preProcessing = (htmlPluginData, callback) => {
-  if (typeof htmlPluginData.plugin.options.preProcessing === 'function') {
-    try {
-      htmlPluginData.html = htmlPluginData.plugin.options.preProcessing(htmlPluginData.html);
-      callback(null, htmlPluginData);
-    } catch(err) {
-      callback(err);
-    }
-  } else {
-    callback(null, htmlPluginData);
-  }
-};
-
-HtmlWebpackProcessingPlugin.prototype.postProcessing = (htmlPluginData, callback) => {
-  if (typeof htmlPluginData.plugin.options.postProcessing === 'function') {
-    try {
-      htmlPluginData.html = htmlPluginData.plugin.options.postProcessing(htmlPluginData.html);
-      callback(null, htmlPluginData);
-    } catch(err) {
-      callback(err);
-    }
-  } else {
-    callback(null, htmlPluginData);
-  }
-};
 
 module.exports = HtmlWebpackProcessingPlugin;
